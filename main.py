ae93cb930a4ec36cadb6a1bdb8847401d644a484
Devin Alexander <devinmalexander@gmail.com>
2025-01-17 05:51:01 -0600

    Python tuples and how to generate random RGB colors

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
 tim = Turtle()
+turtle.colormode(255)
 
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-directions = [0, 90, 180, 270]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+directions = [9, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
-
-for _ in range (200):
-    tim.color(random.choice(colors))
+for _ in range(200):
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
-
 
 
 
