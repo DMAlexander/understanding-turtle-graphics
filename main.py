216489caa1993887268a36d6b281930189ae854c
Devin Alexander <devinmalexander@gmail.com>
2025-01-17 05:43:18 -0600

    Drawing Different Shapes

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range (3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+
+
+
 
 
 
