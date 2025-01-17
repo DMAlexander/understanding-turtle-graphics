1ed30c8cc0852e17b568e86084309bc035b8d834
Devin Alexander <devinmalexander@gmail.com>
2025-01-17 05:54:51 -0600

    Draw spirograph

--- main.py
+++ main.py
@@ -13,16 +13,15 @@ def random_color():
     return random_color
 
 
-directions = [9, 90, 180, 270]
-tim.pensize(15)
 tim.speed("fastest")
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
-
+draw_spirograph(5)
 
 
 
